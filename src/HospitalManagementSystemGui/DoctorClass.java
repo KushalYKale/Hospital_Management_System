e18610d0852730dcde8cd3de21221b2a29d3e4d2
kushal y kale <143431644+KushalYKale@users.noreply.github.com>
2024-09-26 17:49:45 +0530

    Update DoctorClass.java

--- src/HospitalManagementSystemGui/DoctorClass.java
+++ src/HospitalManagementSystemGui/DoctorClass.java
@@ -6,7 +6,7 @@ public class DoctorClass {
 
     private static final String url = "jdbc:mysql://localhost:3306/hospital";
     private static final String username = "root";
-    private static final String password = "Kushal@2004";
+    private static final String password = "password";//add your password
 
     public DoctorClass(){
         viewDoctor();
