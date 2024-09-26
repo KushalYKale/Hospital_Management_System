d7d672a6036a94627f3025e1f578ec0cf688c553
kushal y kale <143431644+KushalYKale@users.noreply.github.com>
2024-09-26 17:48:48 +0530

    Update HospitalManagementSystem.java

--- src/hospitalManagementSystemCLI/HospitalManagementSystem.java
+++ src/hospitalManagementSystemCLI/HospitalManagementSystem.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class HospitalManagementSystem {
     private static final String url = "jdbc:mysql://localhost:3306/hospital";
     private static final String username = "root";
-    private static final String password = "password";
+    private static final String password = "password";//add your password
 
     public static void main(String[] args) {
         try {
